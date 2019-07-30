aeb60c5dfc97066970c246f1e71f35a7e5474f51
João Pascon <joaonatan93@gmail.com>
2019-07-29 21:12:38 -0300

    Mudança nos arquivos

--- files/constants.java
+++ files/constants.java
@@ -1,9 +1,4 @@
 package com.iscoolsync.core.service.util;
-/**
- * Application constants.
- */
 public final class Constants {
-  //Change that field value every build. That change is need to send a correct information to Lambda SyncHeartbeat.
   public static final String SYNC_VERSION = "1.0.1";
-  //Regex for acceptable logins
 }
